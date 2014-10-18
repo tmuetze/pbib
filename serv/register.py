0694c6cce8134fc08b97832ead274514839d16dc
anton <anton@kub>
2014-10-18 19:02:12 +0100

    .

---
+++ serv/register.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from pymongo import MongoClient
+print "Content-Type: text/html"
+print
+
+
+fieldStorage = cgi.FieldStorage(keep_blank_values=1)
+params = {}
+for key in fieldStorage.keys():
+    params[ key ] = fieldStorage[ key ].value
+    if not fieldStorage[ key ].value:
+            params[ key ] = ""
+
+
+client = MongoClient()
+db = client['pbib']
+db.users.insert(params)
+
+print '{"status":"ok"}'
