29129d5c16b34c36760a09a0d3a730b27eb8658b
anton <anton@kub>
2014-10-18 19:20:43 +0100

    .

---
+++ serv/find.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from pymongo import MongoClient
+import cgi, json
+print "Content-Type: text/html"
+print
+
+
+fieldStorage = cgi.FieldStorage()
+params = {}
+for key in fieldStorage.keys():
+    params[ key ] = fieldStorage[ key ].value
+
+client = MongoClient()
+db = client['pbib']
+
+print "["
+# print "records with following filter:" + str(params)
+print
+results = db.users.find(params)
+
+for result in results:
+    del result["_id"]
+    print json.dumps(dict(result), indent=4)
+    print ", "
+print "]"
