e364ac91c8fe9fe362d2054da5d9c96a9bfe9d8d
anton <anton@kub>
2014-10-18 17:17:29 +0100

    adding stuff

---
+++ serv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import BaseHTTPServer
+import CGIHTTPServer
+import cgitb; cgitb.enable()  # # This line enables CGI error reporting
+
+
+server = BaseHTTPServer.HTTPServer
+handler = CGIHTTPServer.CGIHTTPRequestHandler
+server_address = ("", 80)
+handler.cgi_directories = ["/"]
+handler.headers
+httpd = server(server_address, handler)
+
+httpd.serve_forever()
